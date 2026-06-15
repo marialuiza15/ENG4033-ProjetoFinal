3c137ff8b2acb34eae64a18e10278a5dcfb41f58
Danilo De Castro Alves Nascimento <113949433+DaniloCastroA@users.noreply.github.com>
2026-06-15 17:51:48 +0000

    prototipo funcional da musica

--- py/music_player/main.py
+++ py/music_player/main.py
@@ -1,16 +1,23 @@
 import fluidsynth
 import time
 
-SOUNDFONT = "soundfonts/FluidR3_GM.sf2"
+SOUNDFONT = "/usr/share/sounds/sf2/FluidR3_GM.sf2"
 
 fs = fluidsynth.Synth()
-fs.start(driver="pulseaudio")
+
+fs.setting("audio.file.name", "teste.wav")
+fs.setting("audio.file.type", "wav")
+fs.setting("audio.file.format", "s16")
+fs.start(driver="file")
 
 sfid = fs.sfload(SOUNDFONT)
-fs.program_select(0, sfid, 0, 0)  # piano
+fs.program_select(0, sfid, 0, 0)
 
-fs.noteon(0, 60, 100)  # Dó
+fs.noteon(0, 60, 100)
 time.sleep(1)
 fs.noteoff(0, 60)
 
+time.sleep(0.5)
 fs.delete()
+
+print("Arquivo teste.wav criado!")
