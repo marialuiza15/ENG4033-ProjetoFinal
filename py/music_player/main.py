7e454569c75fa0e2c7e1e2d3a1939ea478da58e0
Danilo De Castro Alves Nascimento <113949433+DaniloCastroA@users.noreply.github.com>
2026-06-15 18:54:02 +0000

    delete .wav

--- py/music_player/main.py
+++ py/music_player/main.py
@@ -1,23 +1,28 @@
-import fluidsynth
+import os
 import time
 
-SOUNDFONT = "/usr/share/sounds/sf2/FluidR3_GM.sf2"
+FLUIDSYNTH_BIN = r"C:\Users\micro1\Downloads\fluidsynth\fluidsynth-v2.5.5-win10-x64-cpp11\bin"
+
+os.add_dll_directory(FLUIDSYNTH_BIN)
+os.environ["PATH"] = FLUIDSYNTH_BIN + os.pathsep + os.environ["PATH"]
+
+import fluidsynth
+
+SOUNDFONT = r"C:\Users\micro1\Downloads\FluidR3_GM.sf2"
 
 fs = fluidsynth.Synth()
-
-fs.setting("audio.file.name", "teste.wav")
-fs.setting("audio.file.type", "wav")
-fs.setting("audio.file.format", "s16")
-fs.start(driver="file")
+fs.start(driver="wasapi", midi_driver="none")
 
 sfid = fs.sfload(SOUNDFONT)
 fs.program_select(0, sfid, 0, 0)
+fs.program_select(0, sfid, 0, 0)
 
+fs.noteon(0, 60, 100)
 fs.noteon(0, 60, 100)
 time.sleep(1)
 fs.noteoff(0, 60)
 
-time.sleep(0.5)
+time.sleep(0.2)
 fs.delete()
 
 print("Arquivo teste.wav criado!")
