51c77677c2806ef6b324b33c0f5c871604b2018b
Danilo De Castro Alves Nascimento <113949433+DaniloCastroA@users.noreply.github.com>
2026-06-18 19:51:22 +0000

    add: meta 1 danilo

--- py/music_player/main.py
+++ py/music_player/main.py
@@ -10,19 +10,53 @@ import fluidsynth
 
 SOUNDFONT = r"C:\Users\micro1\Downloads\FluidR3_GM.sf2"
 
+INSTRUMENTOS = {
+    "piano": {"canal": 0, "programa": 0},
+    "baixo": {"canal": 1, "programa": 33},
+    "guitarra": {"canal": 2, "programa": 27},
+    "sintetizador": {"canal": 3, "programa": 89},
+    "orgao": {"canal": 4, "programa": 19},
+    "strings": {"canal": 5, "programa": 48},
+}
+
 fs = fluidsynth.Synth()
 fs.start(driver="wasapi", midi_driver="none")
 
 sfid = fs.sfload(SOUNDFONT)
-fs.program_select(0, sfid, 0, 0)
-fs.program_select(0, sfid, 0, 0)
 
-fs.noteon(0, 60, 100)
-fs.noteon(0, 60, 100)
-time.sleep(1)
-fs.noteoff(0, 60)
+for instrumento in INSTRUMENTOS.values():
+    fs.program_select(instrumento["canal"],sfid,0,instrumento["programa"])
 
-time.sleep(0.2)
+
+sequencia = [
+    {"nota": 60, "inicio": 0, "duracao": 1000, "instrumento": "orgao"},
+    {"nota": 64, "inicio": 0, "duracao": 1000, "instrumento": "orgao"},
+    {"nota": 67, "inicio": 4000, "duracao": 1000, "instrumento": "orgao"},
+]
+
+duracao_total = 0
+while True:
+    seq = fluidsynth.Sequencer(use_system_timer=False)
+    synth_id = seq.register_fluidsynth(fs) 
+    for evento in sequencia:
+        nota = evento["nota"]
+        inicio = evento["inicio"]
+        duracao = evento["duracao"]
+        instrumento = evento["instrumento"]
+
+        configuracao = INSTRUMENTOS[instrumento]
+        canal = configuracao["canal"]
+
+        fim = inicio + duracao
+
+        seq.note_on(inicio, canal, nota, 100, dest=synth_id)
+        seq.note_off(fim, canal, nota, dest=synth_id)
+
+        if fim > duracao_total:
+            duracao_total = fim
+
+    for tempo in range(0, duracao_total + 500, 10):
+        seq.process(tempo)
+        time.sleep(0.01)
+
 fs.delete()
-
-print("Arquivo teste.wav criado!")
